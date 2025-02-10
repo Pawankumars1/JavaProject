71e60729ce08d21e95c02c27066bf104d8dc22d8
Pawan Kumar S <Pawan Kumar S@host.docker.internal>
2025-02-10 13:25:12 +0530

    Initial Commit

---
+++ Backend/src/main/java/com/example/Backend/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.Backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.Backend.model.User;
+import com.example.Backend.repository.UserRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:8081")  
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
